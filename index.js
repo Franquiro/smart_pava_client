d2973210ecd25fb5845946f9fa767552e5c2935d
Franquiro <franquiro@gmail.com>
2020-01-25 04:45:12 -0300

    bootstrap

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
-const servername="http://localhost";
-const port=3000;
+const servername = "http://localhost";
+const port = 3000;
+const slider = document.querySelector("#target_slider");
+const tituloTarget = document.querySelector("#targetTemp");
+function buscarTemps() {
+  let url = `${servername}:${port}/temperaturas`;
+  const tabla = document.querySelector("#temperaturas tbody");
 
-function buscarTemps(){
-    let url = `${servername}:${port}/temperaturas`;
-    const tabla = document.querySelector("#temperaturas tbody");
-    
-    fetch(url)
-        .then(res=>res.json())
-        .then(temperaturas=>{
-            tabla.innerHTML =`
+  fetch(url)
+    .then(res => res.json())
+    .then(temperaturas => {
+      tabla.innerHTML = `
                 <tr>
-                    <th>Timestamp</th>
-                    <th> Temp</th>
+                    <th class="text-center p10-20">Timestamp</th>
+                    <th class="text-center p10-20"> Temp</th>
                 </tr>`;
-                temperaturas.forEach(temp => {
-                    tabla.innerHTML += `
+      temperaturas.forEach(temp => {
+        tabla.innerHTML += `
                         <tr>
-                            <td>${temp.time.split(', ')[1]}</td>
-                            <td>${temp.temp}</td>
+                            <td class="text-center p10-20">${
+                              temp.time.split(" ")[1]
+                            }</td>
+                            <td class="text-center p10-20">${temp.temp}</td>
                         </tr>`;
-                });
-        });
+      });
+    });
 }
 buscarTemps();
 
 const form = document.querySelector("#setTarget");
-form.onsubmit = function(e){
-    e.preventDefault();
+form.onsubmit = function(e) {
+  e.preventDefault();
 
-    const newTarget = {
-        temp : document.querySelector('input[name="temp"]').value,
-        enable : 1
-    };
-    fetch(`${servername}:${port}/target`,{
-        method:'put',
-        body:JSON.stringify(newTarget),
-        headers:{
-            'Content-Type':'application/json'
-        }
-    }).then(res=>res.json())
-    .then(data =>{
-        const tituloTarget = document.querySelector("#targetTemp");
-        tituloTarget.innerHTML = `Target: ${data[0].temp} °${data[0].unit}`;
-        console.log(data);
+  const newTarget = {
+    temp: document.querySelector('input[name="temp"]').value,
+    enable: 1
+  };
+  fetch(`${servername}:${port}/target`, {
+    method: "put",
+    body: JSON.stringify(newTarget),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      tituloTarget.innerHTML = `Target: ${data[0].temp} °${data[0].unit}`;
+      console.log(data);
     });
+};
+function actualizarTarget() {
+  tituloTarget.innerHTML = `Target: ${slider.value}°C`;
 }
-
-setInterval(buscarTemps,500);
+slider.addEventListener("change", actualizarTarget);
+setInterval(buscarTemps, 500);
