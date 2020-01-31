16a6e1a44762a40d9de5943f2730659da0572816
Franquiro <franquiro@gmail.com>
2020-01-31 07:04:16 -0300

    correcciones del timer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const servername = "https://smart-pava.appspot.com";
 const slider = document.querySelector("#target_slider");
 const tituloTarget = document.querySelector("#targetTemp");
+var temps, timer, target;
 function buscarTemps() {
   let url = `${servername}/temperaturas`;
   const tabla = document.querySelector("#temperaturas tbody");
@@ -8,6 +9,7 @@ function buscarTemps() {
   fetch(url)
     .then(res => res.json())
     .then(temperaturas => {
+      temps = temperaturas;
       tabla.innerHTML = `
                 <tr>
                     <th class="text-center p10-20">Timestamp</th>
@@ -29,7 +31,12 @@ buscarTemps();
 const form = document.querySelector("#setTarget");
 form.onsubmit = function(e) {
   e.preventDefault();
-
+  timer = setInterval(() => {
+    buscarTemps();
+    console.log(temps);
+    if (temps[temps.length - 1].temp >= parseInt(slider.value))
+      clearInterval(timer);
+  }, 1000);
   const newTarget = {
     temp: document.querySelector('input[name="temp"]').value,
     enable: 1
@@ -48,7 +55,7 @@ form.onsubmit = function(e) {
     });
 };
 function actualizarTarget() {
-  tituloTarget.innerHTML = `Target: ${slider.value}°C`;
+  target = slider.value;
+  tituloTarget.innerHTML = `Target: ${target} °C`;
 }
 slider.addEventListener("change", actualizarTarget);
-setInterval(buscarTemps, 500);
